f5831c6c97a1a20ac13c8fb2a2b141cba10e5614
louie1386 <louie1386@cycu.org.tw>
2016-03-23 19:15:05 +0800

    debug serial port再次優化
    如題

--- UART_Command/Main/serial.ino
+++ UART_Command/Main/serial.ino
@@ -1,19 +1,24 @@
 char RXD(){
   char rxstr;
   rxstr = Qtserial.read();
-  debug_serial_print(rxstr);
+  debug_serial_print(rxstr, true);
   return rxstr;
   }
 
 char TXD(char txstr){
   Qtserial.print(txstr);
-  debug_serial_print(txstr);
+  debug_serial_print(txstr, false);
   return txstr;
   }
 
-void debug_serial_print(char str){
-  if(byte(str) == 0xAA)
+void debug_serial_print(char str, bool txrx){
+  if(byte(str) == 0xAA){
     debugserial.println(" ");
+    if(txrx)
+      debugserial.print("--> ");
+     else
+      debugserial.print("<-- ");
+    }
   if(byte(str) < 0x10)
     debugserial.print(0);
   debugserial.print(byte(str), HEX);
